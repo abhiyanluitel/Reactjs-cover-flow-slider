35ad3897ae166f1374f35480659ad136b39da3ca
Abhiyan Luitel <abhiyan@mutextech.com.np>
2019-02-15 08:26:26 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+var Coverflow = require('react-coverflow');
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+     <React.Fragment>
+     <Coverflow
+      width={960}
+      height={480}
+      displayQuantityOfSide={2}
+      navigation={false}
+      enableHeading={false}
+    >
+      <div
+        // onClick={() => fn()}
+        // onKeyDown={() => fn()}
+        role="menuitem"
+        tabIndex="0"
+      >
+        <img
+          src= 'Images/IMG1090A.jpg' 
+          alt='this is cover flow slider one1'
+          style={{ display: 'block', width: '100%' }}
+        />
       </div>
-    );
+      <img src='Images/IMG1091A.jpg' alt='Hey this is cover flow sider 2' data-action="http://andyyou.github.io/react-coverflow/"/>
+       <img src='Images/IMG1092A.jpg' alt='Hey this is cover flow slider 3 ' data-action="http://andyyou.github.io/react-coverflow/"/>
+      </Coverflow>, 
+
+
+
+     </React.Fragment>
+    )
   }
 }
 
